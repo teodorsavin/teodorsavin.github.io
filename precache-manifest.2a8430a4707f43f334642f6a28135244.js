c772afa486b9a1d2a9c679fb6f13faeda45f494a
Teodor Savin <t.savin@global.leaseweb.com>
2019-09-23 12:52:02 +0200

    first commit

---
+++ precache-manifest.2a8430a4707f43f334642f6a28135244.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d331353c4dd921607889",
+    "url": "/static/css/main.08c262f9.chunk.css"
+  },
+  {
+    "revision": "d331353c4dd921607889",
+    "url": "/static/js/main.d331353c.chunk.js"
+  },
+  {
+    "revision": "90be9adeabdc23d87ad4",
+    "url": "/static/js/1.90be9ade.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4b475fb3f54b3e35daa04295ec48df4c",
+    "url": "/static/media/linkedin.4b475fb3.svg"
+  },
+  {
+    "revision": "b1c58ee19d1e2c38b29ae86f3174f2e4",
+    "url": "/static/media/instagram-logo.b1c58ee1.svg"
+  },
+  {
+    "revision": "b8ba61fe1f9d0e5c8416ff2713170dad",
+    "url": "/static/media/github.b8ba61fe.svg"
+  },
+  {
+    "revision": "9e19b63b18b9c6ab0c5cd4a9975753ac",
+    "url": "/static/media/background-music.9e19b63b.mp3"
+  },
+  {
+    "revision": "31aae14f159b3e5ab687c0841ce6a8ef",
+    "url": "/static/media/Callous-demo.31aae14f.ttf"
+  },
+  {
+    "revision": "e143945ec40bac029e985505c7b8e0a3",
+    "url": "/static/media/background-min.e143945e.png"
+  },
+  {
+    "revision": "d77798b8fcb1b60a4402edf3193798ca",
+    "url": "/index.html"
+  }
+];
